e8df9b42fa380cd1cdecc4407cb4688d69256342
ACER <ACER@192.168.1.21>
2020-04-14 07:35:09 +0300

    First Commit

---
+++ Homework/src/components/ListDemo.java
@@ -0,0 +1,12 @@
+package components;
+
+import javax.swing.JComponent;
+
+public class ListDemo extends JComponent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
